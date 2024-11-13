d86f2fe0f8c34fbf30a6fca8fcd69b3a56659e3f
Park JaeHong <blogerhong@naver.com>
2024-11-14 01:11:00 +0900

    chore : init commit

---
+++ src/test/java/com/jhpark/schedulerdevelop/SchedulerDevelopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhpark.schedulerdevelop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulerDevelopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
